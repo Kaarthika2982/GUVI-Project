763c009ddc2bbfec396d8dd7851d59fe55eec63f
Kaarthika2982 <91535010+Kaarthika2982@users.noreply.github.com>
2023-11-27 18:43:41 +0530

    register.js

--- js/register.js
+++ js/register.js
@@ -1,19 +1,13 @@
 $(document).ready(function () {
     $("#registerBtn").click(function () {
-        // Validate form before sending AJAX request
         if (validateForm()) {
-            // Get form data
             var formData = $("#registrationForm").serialize();
-
-            // AJAX request to the server
             $.ajax({
                 type: "POST",
                 url: "php/register.php",
                 data: formData,
                 success: function (response) {
-                    // Handle the response from the server
                     if (response === "success") {
-                        // Redirect to login page
                         window.location.href = "login.html";
                     } else {
                         alert("Registration failed. Please try again.");
@@ -24,8 +18,6 @@ $(document).ready(function () {
     });
 
     function validateForm() {
-        // Your existing form validation logic
-        // Ensure all fields are filled, passwords match, etc.
         var password = $("#password").val();
         var confirmPassword = $("#confirmPassword").val();
 
@@ -33,7 +25,6 @@ $(document).ready(function () {
             alert("Passwords do not match");
             return false;
         }
-
         return true;
     }
 });
