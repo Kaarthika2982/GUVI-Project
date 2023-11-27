e14fd08d2690e26bf9538ae5f1395b61d53c3c4c
Kaarthika2982 <91535010+Kaarthika2982@users.noreply.github.com>
2023-11-27 18:39:54 +0530

    connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,13 +1,11 @@
 <?php
-$servername = "localhost"; // usually "localhost" if the database is on the same server
+$servername = "localhost"; 
 $username = "root";
 $password = "";
 $database = "guvi db";
 
-// Create connection
 $mysqli = new mysqli($servername, $username, $password, $database);
 
-// Check connection
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
